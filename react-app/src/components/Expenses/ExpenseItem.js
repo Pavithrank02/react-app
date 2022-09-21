409490f6bf9f22770d464363279003f797226eb9
Pavithran <pavithrank02@gmail.com>
2022-09-21 08:07:04 +0530

    rendering the list

--- react-app/src/components/Expenses/ExpenseItem.js
+++ react-app/src/components/Expenses/ExpenseItem.js
@@ -6,9 +6,9 @@ const ExpenseItem = (props) => {
 
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated');
-  }
+  // const clickHandler = () => {
+  //   setTitle('Updated');
+  // }
     
   return (
     <Card className="expense-item">
@@ -17,7 +17,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
