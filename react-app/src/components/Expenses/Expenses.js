1286ecc3064338146289e6e56b1d2c10d2e5d8c4
Pavithran <pavithrank02@gmail.com>
2022-09-21 19:51:22 +0530

    conditional returns based option

--- react-app/src/components/Expenses/Expenses.js
+++ react-app/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -11,18 +11,16 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          />
-        ))}
+        <ExpensesList items = {filteredExpenses}/>
       </Card>
     </div>
   );
